5c4da67468e1483dfd01948bbab8180bbfe4cae5
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-04-21 17:37:00 +0600

    add input

--- src/views/Catalog.js
+++ src/views/Catalog.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useParams } from "react-router";
+import PageHeader from "../components/PageHeader";
+import MovieGrid from "../components/MovieGrid";
+import { category as cate } from '../api/tmdb';
 
 const Catalog =()=>{
+	const { category } = useParams();
+
 	return (
-		<div>
-			Catalog
-		</div>
-	)
+		 <>
+			  <PageHeader>
+					{category === cate.movie ? 'Movies' : 'TV Series'}
+			  </PageHeader>
+			  <div className="container">
+					<div className="section mb-3">
+						 <MovieGrid category={category}/>
+					</div>
+			  </div>
+		 </>
+	);
 }
 
 export default Catalog;
