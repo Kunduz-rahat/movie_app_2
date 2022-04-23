72153be5ce0e5412b6c282f2d26501ee8afcaf0f
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-04-23 16:33:18 +0600

    add slides of casts

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import tmdbApi from '../../api/tmdb';
 import apiConfig from '../../api/apiConfig';
@@ -18,9 +19,9 @@ const Detail = () => {
 
     useEffect(() => {
         const getDetail = async () => {
-            const response = await tmdbApi.detail(category, id, {params:{}});
+            const response = await tmdbApi.detail(category, id, { params: {} });
             setItem(response);
-            window.scrollTo(0,0);
+            window.scrollTo(0, 0);
         }
         getDetail();
     }, [category, id]);
@@ -30,40 +31,56 @@ const Detail = () => {
             {
                 item && (
                     <>
-                        <div className="banner" style={{backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path || item.poster_path)})`}}></div>
+                        <div className="banner" style={{ backgroundImage: `url(${apiConfig.originalImage(item.backdrop_path || item.poster_path)})` }}></div>
                         <div className="mb-3 movie-content container">
                             <div className="movie-content__poster">
-                                <div className="movie-content__poster__img" style={{backgroundImage: `url(${apiConfig.originalImage(item.poster_path || item.backdrop_path)})`}}></div>
+                                <div className="movie-content__poster__img" style={{ backgroundImage: `url(${apiConfig.originalImage(item.poster_path || item.backdrop_path)})` }}></div>
                             </div>
                             <div className="movie-content__info">
                                 <h1 className="title">
                                     {item.title || item.name}
                                 </h1>
                                 <div className="genres">
+
+
                                     {
                                         item.genres && item.genres.slice(0, 5).map((genre, i) => (
+
                                             <span key={i} className="genres__item">{genre.name}</span>
+
+
                                         ))
                                     }
+
+
                                 </div>
                                 <p className="overview">{item.overview}</p>
                                 <div className="cast">
                                     <div className="section__header">
                                         <h2>Casts</h2>
                                     </div>
-                                    <CastList id={item.id}/>
+                                    <Swiper
+                                        grabCursor={true}
+                                        spaceBetween={10}
+                                        slidesPerView={'auto'}
+                                    >
+                                        <SwiperSlide key={id}>
+                                            <CastList id={item.id} />
+                                        </SwiperSlide>
+                                    </Swiper>
+
                                 </div>
                             </div>
                         </div>
                         <div className="container">
                             <div className="section mb-3">
-                                <VideoList id={item.id}/>
+                                <VideoList id={item.id} />
                             </div>
                             <div className="section mb-3">
                                 <div className="section__header mb-2">
                                     <h2>Similar</h2>
                                 </div>
-                                <MovieList category={category} type="similar" id={item.id}/>
+                                <MovieList category={category} type="similar" id={item.id} />
                             </div>
                         </div>
                     </>
